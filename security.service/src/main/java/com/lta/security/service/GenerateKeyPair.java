65e6f1515a28399f3b458213f61d106a80b0b32c
NelsonOrtiz94 <ortnn990@gmail.com>
2025-07-25 08:55:38 -0500

    Se generan claves priv y pub para evidenciar que la aplicación funciona correctamente

--- security.service/src/main/java/com/lta/security/service/GenerateKeyPair.java
+++ security.service/src/main/java/com/lta/security/service/GenerateKeyPair.java
@@ -18,12 +18,13 @@ public class GenerateKeyPair {
         byte[] priv = keyPair.getPrivate().getEncoded();
 
         // Escribiendo la clave pública
-        PemWriter pemWriter = new PemWriter(new OutputStreamWriter(new FileOutputStream("public.pem")));
+        PemWriter pemWriter = new PemWriter(new OutputStreamWriter(new FileOutputStream("pub.pem")));
         PemObject pemObject = new PemObject("PUBLIC KEY", pub);
         pemWriter.writeObject(pemObject);
         pemWriter.close();
 
-        PemWriter pemWriter2 = new PemWriter(new OutputStreamWriter(new FileOutputStream("public.pem")));
+        // Escribiendo la clave privada
+        PemWriter pemWriter2 = new PemWriter(new OutputStreamWriter(new FileOutputStream("pri.pem")));
         PemObject pemObject2 = new PemObject("PRIVATE KEY", priv);
         pemWriter2.writeObject(pemObject2);
         pemWriter2.close();
